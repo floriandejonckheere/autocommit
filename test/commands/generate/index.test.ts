f37fc290bb79e6a944ef6a68eb979108e778e531
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-05-25 12:52:05 +0200

    Remove hello command

--- test/commands/generate/index.test.ts
+++
@@ -1,9 +0,0 @@
-import {runCommand} from '@oclif/test'
-import {expect} from 'chai'
-
-describe('generate', () => {
-  it('runs generate', async () => {
-    const {stdout} = await runCommand('generate --verbose')
-    expect(stdout).to.contain('Generating verbose commit messages!')
-  })
-})
